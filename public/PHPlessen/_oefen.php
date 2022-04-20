f4cd7608bec987372cc1f7bd91d6650f962d7873
marit-dj <152129@csgaugustinus.nl>
2022-04-20 10:09:26 +0000

    .

--- public/PHPlessen/_oefen.php
+++ public/PHPlessen/_oefen.php
@@ -1,4 +1,11 @@
 <?php
 // met dubbele slashes maak je een commentaarregel
-echo 'Hello <strong>world</strong>!!';
+echo '<strong>Hey world!</strong>';
+$zus=array('voornaam' =>'Myrthe', 'achternaam' => 'de Jong');
+    $zus['geboortejaar']=2004;
+    $zus['sport']='voetbal';
+     
+      
+      echo "<h2>".$zus['voornaam']." ".$zus['achternaam']." is geboren in ".$zus['geboortejaar']." en zit op ".$zus['sport']."</h2>";
+   
 ?>
--- public/PHPlessen/opdracht_3.php
+++ public/PHPlessen/opdracht_3.php
@@ -8,10 +8,20 @@ TYP HIERONDER JOUW PHPCODE
 $waarde=9/10;
 $kwad=pow($waarde,2);
 $rec=1/$kwad;
-$afgerond=round($rec,7);
+$afgerond=round($rec,6);
+
+$afgekapt=floor($rec * 1000000) / 1000000;
 
 echo "Het kwadraat van $waarde is $kwad.<br>
-      Het omgekeerde daarvan is (afgerond:) $afgerond<br>";
+      Het omgekeerde daarvan is (afgerond:) $afgerond<br>
+      Afgekapt is de waarde veel mooier: $afgekapt<br>";
+
+$a=3;
+$b=2.3;
+$c=round(sqrt(pow($a,2)+pow($b,2)),3);
+echo "Een driehoek met rechte zijden $a en $b heeft als schuine zijde (afgerond) $c<br>";
+$mod=fmod($a,$b);
+echo "De restwaarde van de deling $b / $a is $mod";
 
 /****************************
 EINDE VAN JOUW PHPCODE
